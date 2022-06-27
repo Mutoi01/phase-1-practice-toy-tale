84a601aca1fbf572a89ba83e0430b210599cf4bf
Mutoi01 <pngando9@gmail.com>
2022-06-27 04:57:54 +0300

    commit tree 59ae62d3988fa6b4655a61cbaaf9088fecb1ea89

--- src/index.js
+++ src/index.js
@@ -13,3 +13,63 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let like=0
+const inputInfo={ }
+function renderToys(toys){
+  const toyCollection=document.getElementById("toy-collection")
+  const toyCard=document.createElement("div")
+  toyCard.className="card"
+  toyCard.innerHTML=`  <h2>${toys.name}</h2>
+  <img src="${toys.image}" class="toy-avatar" />
+  <p>4 Likes</p>
+  <button class="like-btn" id="${toys.id}">Like ❤️</button>`
+  toyCollection.appendChild(toyCard);
+}
+// get begins
+fetch("http://localhost:3000/toys")
+.then(res=>res.json())
+.then(toysData=> toysData.forEach(toys=>renderToys(toys)))
+
+// post starts here
+const form=document.querySelector(".add-toy-form").addEventListener("submit",(e)=>{
+  e.preventDefault()
+  const inputName=document.querySelector(".input-text").value
+  const inputImg=document.querySelector(".input-text").value
+  inputInfo.name=inputName
+  inputInfo.image=inputImg
+  inputInfo.likes=like
+ 
+
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers:{
+      "content-Type":"application/json",
+      accept:"application/json"
+    },
+    body:JSON.stringify(inputInfo)
+  })
+})
+
+function increaseLikes(toys){
+  let lykBtn=document.getElementById(`${toys.id}`)
+  let likeIncrease=toys.likes
+  console.log(lykBtn)
+  lykBtn.addEventListener("click",(e)=>{ 
+    e.preventDefault();
+
+  let addLike =likeIncrease + 1
+
+  fetch(`http://localhost:3000/toys/${toys.id}`, {
+    method: "PATCH",
+    headers:{
+      "content-Type":"application/json",
+      accept:"applicatio/json"
+    },
+    body:JSON.stringify({
+      likes:"addLike"
+    })
+  })
+  
+  })
+}
